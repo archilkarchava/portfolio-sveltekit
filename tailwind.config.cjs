2eb433e01785fa6d0a23c92d5bf9a072804052b5
Archil Karchava <hello@archil.dev>
2021-09-11 20:00:46 +0500

    feat: add custom "Inter" font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 const config = {
 	mode: "jit",
   darkMode: 'media',
 	purge: [
 		"./src/**/*.{html,js,svelte,ts}",
 	],
-	theme: {
-		extend: {},
-	},
+  theme: {
+    fontFamily: {
+      ...defaultTheme.fontFamily,
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
+  },
 	plugins: [],
 };
 
